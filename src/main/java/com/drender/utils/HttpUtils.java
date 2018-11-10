d3fe9330d0be92dccfb44e8ada62a6b34c5db427
Shantanu Mukherjee <shantanu27.bits@gmail.com>
2018-11-10 12:21:13 -0500

    POST test

--- src/main/java/com/drender/utils/HttpUtils.java
+++ src/main/java/com/drender/utils/HttpUtils.java
@@ -47,7 +47,7 @@ public class HttpUtils {
         logger.info("POST Request: " + domain + ":" + port + uri + " Body: " + Json.encode(requestBody));
 
         client
-            .post(port, domain, uri)
+            .post(port, uri, domain)
             .putHeader("content-type", "application/json")
             .sendJson(requestBody, ar -> {
                 if (ar.succeeded()) {
